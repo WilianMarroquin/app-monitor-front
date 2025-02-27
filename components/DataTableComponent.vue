abebf37b74521a36a2b30cf0a965f950c4714eb7
WilianMarroquin <githubwiliangit@gmail.com>
2025-02-27 17:56:18 -0600

    Mejoras en las funcionalidades relacionadas al generador de CRUD

---
+++ components/DataTableComponent.vue
@@ -0,0 +1,179 @@
+<script lang="ts" setup>
+const { get } = useClienteResquest()
+
+interface Columna {
+  title: string
+  key: string
+}
+
+interface DataTableProps {
+  endpoint: string
+  columnas: Columna[]
+  cantidadPorPagina?: number
+  cantidadPorPaginaOpciones?: number[]
+  botones?: string[]
+  titulo?: string
+  filtros?: object
+  relaciones?: string[]
+  nombreArchivoExport?: string
+}
+
+interface PaginatedResponse {
+  current_page: number
+  data: []
+  last_page: number
+  per_page: number
+  total: number
+}
+
+interface QueryParams {
+  'page[number]'?: number
+  'page[size]'?: number
+  sort?: string
+  include?: string
+  [key: string]: string | number | undefined
+}
+
+const props = defineProps<DataTableProps>();
+
+const cantidadPorPaginaValorDefault: number[] = [10, 20, 30];
+
+const botonesDefault: string[] = ['xlsx', 'pdf', 'csv', 'reiniciar'];
+
+const fecha = new Date();
+
+let {
+  columnas,
+  endpoint,
+  cantidadPorPagina = 10,
+  cantidadPorPaginaOpciones = cantidadPorPaginaValorDefault,
+  botones = botonesDefault,
+  relaciones,
+  titulo = '',
+  nombreArchivoExport = 'reporte_data' + fecha.getTime(),
+
+} = props;
+
+let {filtros} = toRefs(props);
+
+const items = ref<[]>([]);
+const currentPage = ref<number>(1);
+const totalItems = ref<number>(0);
+const loading = ref<boolean>(false);
+const search = ref<string>('');
+let itemsbyPage = ref<number>(cantidadPorPagina);
+let itemsbyPageOptions = ref<number[]>(cantidadPorPaginaOpciones);
+
+const orden = ref([{ key: columnas[0].key, order: 'asc' }] as const)
+
+const getItems = async (): Promise<void> => {
+
+  const sortParam = orden.value.length ? (orden.value[0].order === 'asc' ? orden.value[0].key : `-${orden.value[0].key}`) : ''
+
+  let data: QueryParams = {
+    'page[number]': currentPage.value,
+    'page[size]': itemsbyPage.value,
+    'sort': sortParam,
+    'include': relaciones?.join(','),
+  };
+
+  if (filtros.value) {
+    let key = Object.keys(filtros.value);
+
+    for (let i = 0; i < key.length; i++) {
+      console.log('key[i]', key[i]);
+      data[`filter[${key[i]}]`] = filtros.value[key[i]];
+    }
+
+  }
+
+  try {
+
+    loading.value = true;
+
+    const respuesta: {data: PaginatedResponse} = await get(endpoint, {
+      params: {
+        ...data,
+        search: search.value,
+      },
+    })
+
+    items.value = respuesta.data.data;
+    totalItems.value = respuesta.data.total;
+
+  } catch (error: any) {
+
+    console.log(error)
+
+  } finally {
+
+    loading.value = false;
+
+  }
+};
+getItems();
+
+watch([currentPage, itemsbyPage, filtros], getItems, { deep: true });
+
+const reiniciar = () => {
+
+  currentPage.value = 1;
+  itemsbyPage.value = cantidadPorPagina;
+  filtros.value = {};
+  orden.value = [{ key: columnas[0].key, order: 'asc' }];
+
+  getItems();
+
+};
+
+defineExpose({
+  getItems,
+});
+
+</script>
+
+<template>
+
+  <VCard :title="titulo">
+
+    <VCardText>
+
+      <VRow>
+        <VDataTableServer
+          v-model:items-per-page="itemsbyPage"
+          v-model:page="currentPage"
+          :headers="columnas"
+          :items="items"
+          v-model:sort-by="orden"
+          :items-length="totalItems"
+          :items-per-page-options="itemsbyPageOptions"
+          :loading="loading"
+          :search="search"
+          item-value="id"
+          @update:sort-by="getItems"
+          @update:search="getItems"
+        >
+
+          <template
+            v-for="(slot,name) in $slots"
+            :key="name"
+            v-slot:[name]="slotProps"
+          >
+            <slot :name="name" v-bind="slotProps"></slot>
+          </template>
+
+        </VDataTableServer>
+
+      </VRow>
+
+    </VCardText>
+
+  </VCard>
+
+</template>
+
+<style lang="scss" scoped>
+/* Estilos personalizados */
+</style>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,25 @@
 <script lang="ts" setup>
-const { user } = useSanctumAuth()
 
-console.log(user.value)
-
+const data = ref([
+  {
+    title: 'id',
+    key: 'id',
+  },
+  {
+    title: 'name',
+    key: 'name',
+  },
+])
 
 </script>
 
 <template>
-  <div>
-    <VCard
-      class="mb-6"
-      title="Kick start your project 🚀"
-    >
-      <VCardText>All the best for your new project.</VCardText>
-      <VCardText>
-        Please make sure to read our <a
-          href="https://demos.pixinvent.com/materialize-vuejs-admin-template/documentation/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-decoration-none"
-        >
-          Template Documentation
-        </a> to understand where to go from here and how to use our template.
-      </VCardText>
-    </VCard>
-
-    <VCard title="Want to integrate JWT? 🔒">
-      <VCardText>We carefully crafted JWT flow so you can implement JWT with ease and with minimum efforts.</VCardText>
-      <VCardText>Please read our  JWT Documentation to get more out of JWT authentication.</VCardText>
-    </VCard>
-  </div>
+  <DataTableComponent
+    :columnas="data"
+    endpoint="api/users"
+    :cantidadPorPagina="10"
+    :cantidadPorPaginaOpciones="[10, 20, 30]"
+    :botones="['xlsx', 'pdf', 'csv', 'reiniciar']"
+    titulo="Listado de usuarios"
+  />
 </template>
